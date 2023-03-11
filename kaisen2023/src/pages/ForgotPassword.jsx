4c471a03e9cf3f7f86f20ed6dcec8c3632a1b0c0
SudhansuuRanjan <sudhanshur.ug20.ee@gmail.com>
2023-03-11 11:28:42 +0530

    fixed styling

--- kaisen2023/src/pages/ForgotPassword.jsx
+++ kaisen2023/src/pages/ForgotPassword.jsx
@@ -43,15 +43,16 @@ const ForgotPassword = () => {
 
 
   return (
-    <div className="bg-[url('/images/list-bg.png')] bg-repeat-y  min-h-screen bg-center bg-cover pt-15 flex relative">
-      <div className='bg-[#E9CC7E] border-img  bg-opacity-10 backdrop-blur-sm rounded-xl w-fit bg-center m-auto h-fit flex items-center justify-center flex-col py-10 px-10'>
-        <h1 className='text-4xl font-bold'>Forgot Password</h1>
+    <div className="bg-[url('/images/list-bg.png')] bg-repeat-y  min-h-screen bg-center bg-cover flex flex-col items-center justify-center gap-16 pt-20">
+       <img src="/images/kaizen.png" alt="logo" className='h-16' />
+      <div className='bg-[#E9CC7E] border-img bg-opacity-10 backdrop-blur-sm rounded-xl w-fit bg-center m-auto h-fit flex items-center justify-center flex-col py-10 px-5 md:px-7 lg:px-10 mt-0'>
+        <h1 className='text-3xl lg:text-4xl font-bold'>Forgot Password</h1>
         <form className='m-auto  flex items-center justify-center flex-col' onSubmit={handleSubmit}>
           <div className='m-2 mt-10'>
-            <input id="email" type="email" placeholder="Email" className='py-3 rounded-lg px-3 w-[20rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.email} onChange={onChange} />
+            <input id="email" type="email" placeholder="Email" className='py-2.5 md:py-3 lg:py-3 rounded-lg px-3 lg:w-[20rem] w-[17rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.email} onChange={onChange} />
           </div>
 
-          <button className='bg-yellow-600 py-3 px-10 rounded-lg w-[20rem] m-3 flex items-center justify-center' type='submit'>
+          <button className='bg-yellow-600 py-2.5 md:py-3 lg:py-3 px-2 rounded-lg lg:w-[20rem] w-[17rem] m-3 flex items-center justify-center' type='submit'>
             {loading ? <Loader color="pink-100" size="5" /> : null}
             <span>{loading ? 'Sending Link' : "Send Password Reset Link"}</span>
           </button>
@@ -60,7 +61,7 @@ const ForgotPassword = () => {
         </form>
         <p className='my-5 text-gray-500 text-lg'>or</p>
         <Link to="/signin">
-          <button className='bg-green-600 py-2 px-4  rounded-md bitems-center justify-between w-[13rem] shadow'>
+          <button className='bg-pink-700 py-2 px-4  rounded-md bitems-center justify-between w-[13rem] shadow'>
             <p>SignIn</p>
           </button>
         </Link>
--- kaisen2023/src/pages/SignUp.jsx
+++ kaisen2023/src/pages/SignUp.jsx
@@ -67,18 +67,19 @@ const SignUp = () => {
 
 
   return (
-    <div className="bg-[url('/images/list-bg.png')] bg-repeat-y  min-h-screen bg-center bg-cover pt-15 flex relative">
-      <div className='bg-[#E9CC7E] border-img  bg-opacity-10 backdrop-blur-sm rounded-xl w-fit bg-center m-auto h-fit flex items-center justify-center flex-col py-10 px-10'>
-        <h1 className='text-4xl font-bold'>SignUp</h1>
+    <div className="bg-[url('/images/list-bg.png')] bg-repeat-y  min-h-screen bg-center bg-cover flex flex-col items-center justify-center gap-10 pt-20">
+      <img src="/images/kaizen.png" alt="logo" className='h-16' />
+      <div className='bg-[#E9CC7E] border-img bg-opacity-10 backdrop-blur-sm rounded-xl w-fit bg-center m-auto h-fit flex items-center justify-center flex-col py-10 px-5 md:px-7 lg:px-10 mt-0'>
+        <h1 className='text-3xl lg:text-4xl font-bold'>SignUp</h1>
         <form className='m-auto  flex items-center justify-center flex-col' onSubmit={handleSubmit}>
           <div className='m-2 mt-10'>
-            <input id="name" type="name" placeholder="Name" className='py-3 rounded-lg px-3 w-[20rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.name} onChange={onChange} />
+            <input id="name" type="name" placeholder="Name" className='py-2.5 md:py-3 lg:py-3 rounded-lg px-3 lg:w-[20rem] w-[17rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.name} onChange={onChange} />
           </div>
           <div className='m-2'>
-            <input id="email" type="email" placeholder="Email" className='py-3 rounded-lg px-3 w-[20rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.email} onChange={onChange} />
+            <input id="email" type="email" placeholder="Email" className='py-2.5 md:py-3 lg:py-3 rounded-lg px-3 lg:w-[20rem] w-[17rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.email} onChange={onChange} />
           </div>
           <div className='m-2 flex items-center relative'>
-            <input id="password" type={showPassword ? "text" : "password"} placeholder="Password" className='py-3 rounded-lg px-3 w-[20rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.password} onChange={onChange} />
+            <input id="password" type={showPassword ? "text" : "password"} placeholder="Password" className='py-2.5 md:py-3 lg:py-3 rounded-lg px-3 lg:w-[20rem] w-[17rem] border-2 border-yellow-400 bg-yellow-600 bg-opacity-10' value={formData.password} onChange={onChange} />
             <button onClick={(e) => {
               e.preventDefault();
               setShowPassword((prevState) => !prevState);
@@ -88,7 +89,7 @@ const SignUp = () => {
             </button>
           </div>
 
-          <button className='bg-yellow-600 py-3 px-10 rounded-lg w-[20rem] m-3 flex items-center justify-center' type='submit'>
+          <button className='bg-yellow-600 py-2.5 md:py-3 lg:py-3 px-10 rounded-lg lg:w-[20rem] w-[17rem] m-3 flex items-center justify-center' type='submit'>
             {loading ? <Loader color="pink-100" size="5" /> : null}
             <span>{loading ? 'Creating Account' : "Sign Up"}</span>
           </button>
