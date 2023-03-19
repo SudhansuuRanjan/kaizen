d6aa5d48110ed985f5833f293da4fd638ddff643
SudhansuuRanjan <sudhanshur.ug20.ee@gmail.com>
2023-03-19 22:40:31 +0530

    fixed bug

--- kaisen2023/src/components/HomePage/NavBar.jsx
+++ kaisen2023/src/components/HomePage/NavBar.jsx
@@ -19,14 +19,16 @@ const NavBar = () => {
     return (
         <>
             <div className='z-[100] fixed top-5 right-5 flex w-fit h'>
-                <button className={`text-lg text-gray-600 transform font-bold transition-all delay-100 ease-in-out ${active && 'rotate-180'} `} onClick={handleClick}>
+                <button
+                    className={`text-lg text-gray-600 transform font-bold transition-all delay-100 ease-in-out ${active && 'rotate-180'}`}
+                    onClick={handleClick}>
                     {
                         active ? <HiXMark size={42} /> : <HiBars2 size={42} />
                     }
                 </button>
             </div>
 
-            <div className={`h-[100%] w-[100%] fixed bg-gray-900 z-10 transform transition-all delay-100 ease-in-out translate-x-[0%] ${!active && 'translate-x-[-110%]'}`}>
+            <div className={`h-[100%] w-[100%] fixed bg-gray-900 z-10 transform transition-all delay-100 ease-in-out ${!active && 'translate-y-[-100%]'}`}>
                 <h1>efjvikwvlwifw43f</h1>
             </div>
         </>
