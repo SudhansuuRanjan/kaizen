39d3db5d5bf3cb0c498fc83d0b3f2d60f9b9c808
SudhansuuRanjan <sudhanshur.ug20.ee@gmail.com>
2023-03-21 20:23:05 +0530

    😎: (fix) : Fixed bug in react-countup component

--- kaisen2023/src/components/HomePage/Highlights.jsx
+++ kaisen2023/src/components/HomePage/Highlights.jsx
@@ -15,22 +15,22 @@ const Highlights = () => {
             <div className='grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-5 items-center justify-between lg:h-[20rem] md:h-[20rem] h-fit m-auto lg:w-[55rem] md:w-[90%] w-[80%]'>
                 <div className='text-center py-5 flex flex-col items-center justify-center gap-3 bg-black w-[100%] border-b-4 border-b-red-600 hover:border-b-red-700 rounded-xl  bg-opacity-30 hover:bg-opacity-50 backdrop-blur-sm transition-all delay-100 ease-in-out hover:mb-1 z-0 hover:z-10'>
                     <SiGithubsponsors size={42} className="text-red-500 text-3xl my-2 transition-all ease-in delay-100 hover:rotate-180" />
-                    <h3 className='text-4xl font-bold'><CountUp start={0} end={100} enableScrollSpy={true} />+</h3>
+                    <h3 className='text-4xl font-bold'><CountUp end={100} enableScrollSpy={true} />+</h3>
                     <p className='text-xl'>Years</p>
                 </div>
                 <div className='text-center py-5 flex flex-col items-center justify-center gap-3 bg-black w-[100%] border-b-4 border-b-red-600 hover:border-b-red-700 rounded-xl  bg-opacity-30 hover:bg-opacity-50 backdrop-blur-sm transition-all delay-100 ease-in-out hover:mb-1 z-0 hover:z-10'>
                     <FiSmile size={42} className="text-red-500 text-3xl my-2 transition-all ease-in delay-100 hover:rotate-180" />
-                    <h3 className='text-4xl font-bold'><CountUp start={0} end={200} enableScrollSpy={true} />+</h3>
+                    <h3 className='text-4xl font-bold'><CountUp end={200} enableScrollSpy={true} />+</h3>
                     <p className='text-xl'>Years</p>
                 </div>
                 <div className='text-center py-5 flex flex-col items-center justify-center gap-3 bg-black w-[100%] border-b-4 border-b-red-600 hover:border-b-red-700 rounded-xl  bg-opacity-30 hover:bg-opacity-50 backdrop-blur-sm transition-all delay-100 ease-in-out hover:mb-1 z-0 hover:z-10'>
                     <AiOutlineStar size={42} className="text-red-500 text-3xl my-2 transition-all ease-in delay-100 hover:rotate-180" />
-                    <h3 className='text-4xl font-bold'><CountUp start={0} end={1000} enableScrollSpy={true} />+</h3>
+                    <h3 className='text-4xl font-bold'><CountUp end={1000} enableScrollSpy={true} />+</h3>
                     <p className='text-xl'>Years</p>
                 </div>
                 <div className='text-center py-5 flex flex-col items-center justify-center gap-3 bg-black w-[100%] border-b-4 border-b-red-600 hover:border-b-red-700 rounded-xl  bg-opacity-30 hover:bg-opacity-50 backdrop-blur-sm transition-all delay-100 ease-in-out hover:mb-1 z-0 hover:z-10'>
                     <GiSandsOfTime size={42} className="text-red-500 text-3xl my-2 transition-all ease-in delay-100 hover:rotate-180" />
-                    <h3 className='text-4xl font-bold'><CountUp start={0} end={21} enableScrollSpy={true} />+</h3>
+                    <h3 className='text-4xl font-bold'><CountUp end={21} enableScrollSpy={true} />+</h3>
                     <p className='text-xl'>Years</p>
                 </div>
             </div>
