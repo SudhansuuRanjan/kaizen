590093c738846c7a80f2c17fabb0995b4d60e5bb
SudhansuuRanjan <sudhanshur.ug20.ee@gmail.com>
2023-04-30 19:14:07 +0530

    fixed bugs

--- kaisen2023/src/pages/GetPasses/Pass.jsx
+++ kaisen2023/src/pages/GetPasses/Pass.jsx
@@ -66,7 +66,7 @@ const Pass = () => {
                     <div className='h-full w-full bg-black bg-opacity-80 p-5'>
                         <p className='pb-2 text-yellow-400 text-base'>This Basic Registration includes:</p>
                         <ul className='list-inside'>
-                            <li>✅Lunch on all 5 days</li>
+                            {/* <li>✅Lunch on all 5 days</li> */}
                             <li>✅ Kaizen Merchandise & Goodies</li>
                             <li>✅ Access to all the events</li>
                             <li>✅ A seat in Medical Workshops lead under eminent faculty of AIIMS Patna:</li>
