b8e533432d7a6ab6b6d647d7a17c45bb8c4a849c
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2023-05-12 12:54:32 +0530

    Updated admin access

--- kaizen-admin/src/pages/CreatePass/CreatePass.jsx
+++ kaizen-admin/src/pages/CreatePass/CreatePass.jsx
@@ -23,7 +23,10 @@ const CreatePass = () => {
 
     const allowedUsers = [
         'sudhanshuranjan2k18@gmail.com',
-        'deepakranjan488@gmail.com'
+        'deepakranjan488@gmail.com',
+        'prachi@aiimspatna.org',
+        'rudrapriya@aiimspatna.org',
+        'abhinavkumar@aiimspatna.org'
     ]
 
     const auth = getAuth();
